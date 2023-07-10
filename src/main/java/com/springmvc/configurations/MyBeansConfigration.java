306d43a738813a2622ece659fcecbdf2f077ad94
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-10 15:22:49 +0530

    converted to beans configuration class

--- src/main/java/com/springmvc/configurations/MyBeansConfigration.java
+++ src/main/java/com/springmvc/configurations/MyBeansConfigration.java
@@ -1,6 +1,21 @@
 package com.springmvc.configurations;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
-public class MyBeansConfigration {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.springmvc")
+public class MyBeansConfigration implements WebApplicationInitializer{
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		
+	}
 
 }
