df346fa0d06c331e74b79c9467931390530867dc
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-10 15:28:08 +0530

    implemrnted WebApplicationInitializer

--- src/main/java/com/springmvc/configurations/MyWebconfiguration.java
+++ src/main/java/com/springmvc/configurations/MyWebconfiguration.java
@@ -1,5 +1,16 @@
 package com.springmvc.configurations;
 
-public class MyWebconfiguration {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class MyWebconfiguration implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		
+		
+	}
 
 }
