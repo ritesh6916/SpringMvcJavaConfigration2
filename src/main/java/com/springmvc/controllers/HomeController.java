6ab7b8a910111ceebfc5ad1ed92ae70081acfee8
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-10 15:53:29 +0530

    basic controller

--- src/main/java/com/springmvc/controllers/HomeController.java
+++ src/main/java/com/springmvc/controllers/HomeController.java
@@ -1,5 +1,16 @@
 package com.springmvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
 
+	@ResponseBody
+	@RequestMapping("test")
+	public String homeController()
+	{
+		return "Home";
+	}
 }
